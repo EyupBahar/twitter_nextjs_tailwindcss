f93f0a8b1417a6ec6ec54ec27a4f4fc782ee6e67
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-07 22:05:16 +0300

    add sidebar tweetbox updates

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -7,9 +7,11 @@ interface Props {
 
 const SidebarRow = ({ Icon, title }: Props) => {
   return (
-    <div className="hover: transation-all group flex cursor-pointer items-center space-x-2 rounded-full px-4 py-3 duration-200 hover:bg-gray-100">
+    <div className="group:flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100">
       <Icon className="h-6 w-6" />
-      <p className="hidden group-hover:text-twitter md:inline-flex">{title} </p>
+      <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}{' '}
+      </p>
     </div>
   )
 }
