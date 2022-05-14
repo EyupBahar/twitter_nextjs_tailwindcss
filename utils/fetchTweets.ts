50aaa496187316d482a156703e73e3fe3f552eb1
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-15 00:01:11 +0300

    fetchData updates

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,1 +1,6 @@
-export const fetchTweets = async () => {}
+export const fetchTweets = async () => {
+  const res = await fetch('${process.env.PUBLIC_BASE_URL}/api/getTweets')
+
+  const data = await res.json()
+  const tweets: Tweet[] = data.tweets
+}
